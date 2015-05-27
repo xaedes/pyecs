798407131e3a71d898bda2f416e55277b8e90757
xaedes <xaedes@gmail.com>
2015-05-27 14:20:02 +0200

    pyecs package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name="pyecs",
+	version="0.1",
+	description="Python Entity Component System Framework inspired from Unity",
+	url="http://github.com/xaedes/pyecs",
+	author="xaedes",
+	author_email="xaedes@gmail.com",
+	license="MIT",
+	packages=["pyecs"],
+	zip_safe=False
+	)
