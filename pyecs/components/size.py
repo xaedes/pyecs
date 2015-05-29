5c2ae705d04251e569a17a96a83500a038d974d0
xaedes <xaedes@gmail.com>
2015-05-29 12:34:19 +0200

    return boundingbox of size

--- pyecs/components/size.py
+++ pyecs/components/size.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 from __future__ import division    # Standardmäßig float division - Ganzzahldivision kann man explizit mit '//' durchführen
+from numbers import Number
+
 from pyecs import *
+from pyecs.components import Pose
 
 class Size(Component):
     """docstring for Size"""
     def __init__(self, size, *args,**kwargs):
         super(Size, self).__init__(*args,**kwargs)
         self.size = size
+
+    @with_components(required=[Pose])
+    def bounding_box(self, pose):
+    	if isinstance(self.size, Number):
+    		# one-dimensional size
+    		return (pose.x - self.size/2, pose.y - self.size/2, self.size, self.size)
+    	elif type(self.size) == tuple:
+    		# two-dimensional size
+    		return (pose.x - self.size[0]/2, pose.y - self.size[1]/2, self.size[0], self.size[1])
