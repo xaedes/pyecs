2f5ad30ac123706c9ed6000ee4f08b795e252328
xaedes <xaedes@gmail.com>
2015-05-30 11:45:19 +0200

    added components.Size.bounding_circle

--- pyecs/components/size.py
+++ pyecs/components/size.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import division    # Standardmäßig float division - Ganzzahldivision kann man explizit mit '//' durchführen
 from numbers import Number
+import math
 
 from pyecs import *
 from pyecs.components import Pose
@@ -20,3 +21,16 @@ class Size(Component):
     	elif type(self.size) == tuple:
     		# two-dimensional size
     		return (pose.x - self.size[0]/2, pose.y - self.size[1]/2, self.size[0], self.size[1])
+
+    def bounding_circle(self):
+        # get bounding box
+        bbox = self.bounding_box()
+        x,y,w,h = bbox
+        w2,h2 = w/2,h/2
+        # center of bounding box
+        mx,my = x+w2,y+h2
+        # radius of enclosing circle is distance from
+         # center to any of the corner points of the bbox
+        r = math.sqrt(w2*w2+h2*h2)
+        
+        return ((mx,my),r)
