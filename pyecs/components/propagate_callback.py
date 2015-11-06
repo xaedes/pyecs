e912793aacc74c23ab8a78f79e9415aac3ad838b
xaedes <xaedes@gmail.com>
2015-11-06 23:32:52 +0100

    added PropagateCallback.__str__

--- pyecs/components/propagate_callback.py
+++ pyecs/components/propagate_callback.py
@@ -10,11 +10,12 @@ class PropagateCallback(Component):
     """docstring for PropagateCallback"""
     def __init__(self, key, *args,**kwargs):
         super(PropagateCallback, self).__init__(*args,**kwargs)
-
         if type(key) == list:
+            self.keys = key
             for k in key:
                 self.prop(k)
         else:
+            self.keys = [key]
             self.prop(key)
 
     def prop(self, key):
@@ -24,3 +25,7 @@ class PropagateCallback(Component):
                 child.fire_callbacks(key, *args,**kwargs)
 
         self.__dict__[key] = types.MethodType(propagate, self)
+
+
+    def __str__(self):
+        return "%s(%s)" % (super(type(self),self).__str__(), ",".join(self.keys))
